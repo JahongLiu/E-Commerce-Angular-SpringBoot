1f47d10f77fff54dae6958c2339916e2139fe4e4
JahongLiu <jahongliu@gmail.com>
2023-08-10 15:44:05 -0400

    review loops and array in typescript

---
+++ TYPESCRIPT_TRAINING/03-loops-and-arrays/growable-arrays.js
@@ -0,0 +1,7 @@
+var sportTwo = ["Gold", "Cricket", "Tennis"];
+sportTwo.push("Baseball");
+sportTwo.push("Futball");
+for (var _i = 0, sportTwo_1 = sportTwo; _i < sportTwo_1.length; _i++) {
+    var tempSport = sportTwo_1[_i];
+    console.log(tempSport);
+}
---
+++ TYPESCRIPT_TRAINING/03-loops-and-arrays/growable-arrays.ts
@@ -0,0 +1,8 @@
+let sportTwo: string[] = ["Gold", "Cricket", "Tennis"];
+
+sportTwo.push("Baseball");
+sportTwo.push("Futball");
+
+for (let tempSport of sportTwo) {
+  console.log(tempSport);
+}
